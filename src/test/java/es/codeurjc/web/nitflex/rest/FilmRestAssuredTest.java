17aaea6d763b33c62205c1bc58d227f2b1ef9c33
Daviid24x <estebanbernardodavid1@gmail.com>
2025-04-21 19:49:07 +0200

    rest 2 more test

--- src/test/java/es/codeurjc/web/nitflex/rest/FilmRestAssuredTest.java
+++ src/test/java/es/codeurjc/web/nitflex/rest/FilmRestAssuredTest.java
@@ -13,6 +13,7 @@ import es.codeurjc.web.nitflex.model.User;
 import es.codeurjc.web.nitflex.repository.UserRepository;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FilmRestAssuredTest {
@@ -38,6 +39,44 @@ public class FilmRestAssuredTest {
     }
 
     @Test
+    public void whenAddFilmWithoutImage_thenFilmCanBeRetrievedById() {
+        String jsonBody = "{"
+            + "\"title\": \"Test Film\","
+            + "\"synopsis\": \"A test film without an image\","
+            + "\"releaseYear\": 2023,"
+            + "\"ageRating\": \"+12\""
+            + "}";
+
+        // Extract the ID as Integer 
+        Integer createdFilmId = 
+            given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(jsonBody)
+            .when()
+                .post("/api/films/")
+            .then()
+                .statusCode(HttpStatus.CREATED.value()) // Verify the status code is created
+                .extract()
+                .path("id");
+
+        // Verify details
+        given()
+            .when()
+                .get("/api/films/" + createdFilmId)
+            .then()
+                .statusCode(HttpStatus.OK.value()) 
+                .body("id", equalTo(createdFilmId)) 
+                .body("title", equalTo("Test Film")) 
+                .body("synopsis", equalTo("A test film without an image")) 
+                .body("releaseYear", equalTo(2023)) 
+                .body("ageRating", equalTo("+12")); 
+
+        System.out.println("REST TEST PASSED! Film created and retrieved successfully by ID.");
+    }
+
+
+
+    @Test
     public void whenAddFilmWithoutTitle_thenErrorMessageIsShown() {
         // Create a film request without title
         String jsonBody = "{"
@@ -58,5 +97,45 @@ public class FilmRestAssuredTest {
             .body(containsString("The title is empty"));
 
         System.out.println("REST TEST PASSED! Error message shown when creating film without title");
+    }
+
+
+    @Test
+    public void whenAddAndDeleteFilm_thenFilmIsNotAvailable() {
+        // Create the request body for a new film
+        String jsonBody = "{"
+            + "\"title\": \"Test Film to Delete\","
+            + "\"synopsis\": \"A test film that will be deleted\","
+            + "\"releaseYear\": 2023,"
+            + "\"ageRating\": \"+12\""
+            + "}";
+
+        // Send the POST request to create the film
+        Integer createdFilmId = 
+            given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(jsonBody)
+            .when()
+                .post("/api/films/")
+            .then()
+                .statusCode(HttpStatus.CREATED.value()) // Verify the status code is 201 Created
+                .extract()
+                .path("id");
+
+        // Send the DELETE request to remove the film
+        given()
+            .when()
+                .delete("/api/films/" + createdFilmId)
+            .then()
+                .statusCode(HttpStatus.NO_CONTENT.value()); // Verify the status code is 204 No Content
+
+        // Attempt to retrieve the deleted film
+        given()
+            .when()
+                .get("/api/films/" + createdFilmId)
+            .then()
+                .statusCode(HttpStatus.NOT_FOUND.value()); // Verify the status code is 404 Not Found
+
+        System.out.println("REST TEST PASSED! Film created, deleted, and confirmed unavailable.");
     }
 }
